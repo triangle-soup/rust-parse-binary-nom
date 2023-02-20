cf795e14581ed51bdc31d1e06a52df12be509fd6
neil <neil@thethirdguild.com>
2023-02-20 14:59:46 +0000

    style: fmt

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::{Result, Error};
+use anyhow::{Error, Result};
 mod reader;
 use reader::{check_file_extension, read_file_to_buffer};
 mod geom;
@@ -18,23 +18,20 @@ fn main() -> Result<()> {
 
     let buffer = read_file_to_buffer(filename)?;
     println!("Bytes read: {:#?}", buffer.len());
-    if ! is_lxob(&buffer) {
-        return Err(Error::msg("LXOB file identifier not found.")); 
-    } 
+    if !is_lxob(&buffer) {
+        return Err(Error::msg("LXOB file identifier not found."));
+    }
 
-    let (_, header) = parse_file_header(&buffer).map_err(|e| {
-        Error::msg(format!("Error parsing header: {e}"))
-    })?;
+    let (_, header) =
+        parse_file_header(&buffer).map_err(|e| Error::msg(format!("Error parsing header: {e}")))?;
     println!("Header: {header:#?}");
 
-    let (_, chunks) = parse_chunk_headers(&buffer).map_err(|e| {
-        Error::msg(format!("Error parsing chunk headers: {e}"))
-    })?;
+    let (_, chunks) = parse_chunk_headers(&buffer)
+        .map_err(|e| Error::msg(format!("Error parsing chunk headers: {e}")))?;
     println!("Chunk count: {:#?}", chunks.len());
 
-    let (_, pnts_chunk) = parse_chunk_pnts(&buffer).map_err(|e| {
-        Error::msg(format!("Error parsing PNTS chunk: {e}"))
-    })?;
+    let (_, pnts_chunk) = parse_chunk_pnts(&buffer)
+        .map_err(|e| Error::msg(format!("Error parsing PNTS chunk: {e}")))?;
     println!("Points {pnts_chunk:#?}");
 
     Ok(())
